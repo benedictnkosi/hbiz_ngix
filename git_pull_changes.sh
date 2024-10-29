c7e3b8904e4d07e954c59c5d8a884a1459e26798
BenedictNkosi01 <Benedict.nkosi@merlinentertainments.biz>
2024-10-29 22:24:53 +0200

    Refactor git_pull_changes.sh to reset local branch and pull latest changes from remote main branch

--- git_pull_changes.sh
+++ git_pull_changes.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 
-git pull
+# Reset the local branch to match the remote main branch
+git reset --hard origin/main
+
+# Pull the latest changes from the remote main branch
+git pull origin main
+
 
 # Move all files to the parent directory
 mv -f * ../
